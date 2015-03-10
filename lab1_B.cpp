637588d71029cd4428c51c03df596a98b3f9d5ab
yan-ang <F74032031@mail.ncku.edu.tw>
2015-03-10 18:10:06 +0800

    homework

---
+++ lab1_B.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n;
+cin>> n;
+cout<< n<<" ";
+
+while(n!=1)
+{	
+	if((n%2)==1)
+	{
+	n=3*n+1;
+	cout<<n<<" ";
+	}
+	else
+	{
+	n=n/2;
+	cout<<n<<" ";
+	}
+}
+cout<<"\n";
+return 0;
+}
